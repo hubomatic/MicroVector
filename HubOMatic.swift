d10e1dbc3cf225e0b89b2c5be5d719c90e1f4789
marcprux <marc@glimpse.io>
2021-02-12 22:09:50 -0500

    'Checkpoint'

--- HubOMatic.swift
+++ HubOMatic.swift
@@ -11,25 +11,6 @@ import SwiftUI
 
 // import HubOMatic // TODO: re-import
 
-public extension View {
-    /// - Note: the current environment much have a `HubOMatic` environment object
-    func withHub(oMatic component: HubOMatic.Component) -> some View {
-        self
-    }
-}
-
-public extension HubOMatic {
-    enum Component {
-        case settingsPanel
-        case checkForUpdateButton
-        case viewReleaseNotesPanel
-    }
-
-    @discardableResult static func start(update: URL, artifact: String, title: String = loc("An update is available for installation"), updateTitle: String = loc("Update"), cancelTitle: String = loc("Cancel")) -> Self {
-        HubOMatic()
-    }
-}
-
 public struct HubOMatic {
     struct Config : Hashable, Codable {
         var repository: URL
@@ -39,4 +20,37 @@ public struct HubOMatic {
 
     public init() {
     }
+}
+
+public extension HubOMatic {
+    @discardableResult static func start(update: URL, artifact: String, title: String = loc("An update is available for installation"), updateTitle: String = loc("Update"), cancelTitle: String = loc("Cancel")) -> Self {
+        HubOMatic()
+    }
+}
+
+public extension HubOMatic {
+    struct UpdateAvailableBannerView : View {
+        public var body: some View {
+            wip(EmptyView())
+        }
+    }
+
+    struct CheckForUpdateButton : View {
+        public var body: some View {
+            wip(EmptyView())
+        }
+    }
+
+    struct UpdateSettingsView : View {
+        public var body: some View {
+            wip(EmptyView())
+        }
+    }
+
+    struct ReleaseNotesPreviewView : View {
+        public var body: some View {
+            wip(EmptyView())
+        }
+    }
+
 }
