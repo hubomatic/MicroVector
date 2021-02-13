af5229de74c3ea89002d096a507f6339db7f2256
marcprux <marc@glimpse.io>
2021-02-12 21:47:42 -0500

    'Checkpoint'

--- HubOMatic.swift
+++ HubOMatic.swift
@@ -7,9 +7,24 @@
 
 import Foundation
 import MiscKit
+import SwiftUI
+
 // import HubOMatic // TODO: re-import
 
+public extension View {
+    /// - Note: the current environment much have a `HubOMatic` environment object
+    func withHub(oMatic component: HubOMatic.Component) -> some View {
+        self
+    }
+}
+
 public extension HubOMatic {
+    enum Component {
+        case settingsPanel
+        case checkForUpdateButton
+        case viewReleaseNotesPanel
+    }
+
     @discardableResult static func start(update: URL, artifact: String, title: String = loc("An update is available for installation"), updateTitle: String = loc("Update"), cancelTitle: String = loc("Cancel")) -> Self {
         HubOMatic()
     }
@@ -23,7 +38,5 @@ public struct HubOMatic {
     var text = "Hello, World!"
 
     public init() {
-
     }
 }
-
